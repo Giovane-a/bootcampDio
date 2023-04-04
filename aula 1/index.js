0148bba1ee5abdf3f207f0051e1f623d617236d2
Giovane Antonio de paiva <giovane-a.p@hotmail.com>
2023-04-03 22:09:11 -0300

    Create index.js

---
+++ aula 1/index.js
@@ -0,0 +1,16 @@
+
+let precoCombustivel = 4.77;
+
+let quilometrosPorlitro = 10;
+
+let kmsRodados = 500;
+
+let litrosConsumidos = kmsRodados / quilometrosPorlitro;
+
+let valorGasto = litrosConsumidos * precoCombustivel
+
+
+console.log(litrosConsumidos) 
+
+console.log (valorGasto.toFixed(2))
+
